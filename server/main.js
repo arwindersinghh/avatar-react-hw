d21ad07d2e0ea0a324a4e6d94fa6e06bb7244dc4
Arwinder Singh <arwindersinghh@gmail.com>
2020-12-10 18:55:03 -0500

    error with webpack

---
+++ server/main.js
@@ -0,0 +1,17 @@
+const server = require('/index')
+const { db } = require('/db')
+
+const init = async() => {
+    try{
+        await syncAndSeed();
+        const port = process.env.PORT || 3000;
+        server.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        });
+    }
+    catch(ex){
+        console.log(ex);
+    }
+}
+
+init();
